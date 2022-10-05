4d100ed4bd8b905993eaf456b070b4074c124071
kaique <kaiquesilva49@hotmail.com>
2022-10-05 14:07:08 -0300

    init

---
+++ src/test/java/microsservices/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package microsservices.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
